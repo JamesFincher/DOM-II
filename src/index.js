9a087956cb58881e327a2aff53f4b5959fcd29d7
heartandkarma <heartandkarma@gmail.com>
2022-03-08 08:28:19 -0800

    got 1 working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Your code goes here!
+
+window.onload = function (e) {
+	console.log(`event ${e.type} executed! Fun Bus Page loaded successfully!`);
+	const heading = document.querySelector("h1");
+	heading.style.backgroundColor = "yellow";
+};
